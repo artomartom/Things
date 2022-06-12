968f484151fcbb539d70bcca4fb9d951ba66a66f
artomartom <1tematematema@gmail.com>
2022-06-12 22:58:23 +0300

    impled lcs

--- LCS/main.cpp
+++ LCS/main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 using std::cout;
 using std::max;
+using std::string;
 using std::string_view;
 using std::vector;
 
@@ -20,16 +22,10 @@ std::ostream &operator<<(std::ostream &os, const vector<T> &vec)
     return os << '\n';
 }
 
-int main(int argv, char *args[])
+vector<vector<int>> LCS(string_view left, string_view right)
 {
-    if (!(argv >= 3))
-    {
-        return -1;
-        cout << "lol";
-    };
-
-    const string_view left{args[1]};  // n
-    const string_view right{args[2]}; // m
+    if (left.length() == 0 || right.length() == 0)
+        return {};
     vector<vector<int>> mat(
         right.length() + 1,            // add 1 col for "" cast
         vector<int>(left.length() + 1, // add 1 row for "" cast
@@ -46,15 +42,47 @@ int main(int argv, char *args[])
              ++row)
         {
             char n{left[row - 1]};
-            mat[col][row] = std::max(mat[col - 1][row], mat[col][row - 1]);
             if (n == m)
             {
-                mat[col][row] += 1;
-                // cout << '[' << n << m << ']'<<'\n';
+                mat[col][row] = 1 + mat[col - 1][row - 1];
+            }
+            else
+            {
+                mat[col][row] = std::max(mat[col - 1][row], mat[col][row - 1]);
             }
         };
     };
+    return mat;
+};
+
+int main(int argv, char *args[])
+{
+    if (!(argv >= 3))
+    {
+        return -1;
+        cout << "lol";
+    };
+
+    const string_view left{args[1]};  // n
+    const string_view right{args[2]}; // m
+    string common{""};
+    cout << "'" << left << "', '" << right << "'\n";
+
+    auto mat{LCS(left, right)};
     for (auto &vec : mat)
         cout << vec;
-    //   cout << left << ' ' << right;
+    cout << "LSC: " << mat.back().back() << '\n';
+
+    auto begin{mat.back().rbegin()};
+    auto end{mat.back().rend()};
+    auto target{*end};
+
+    for (; begin != end ;++begin)
+    {
+        //if (*begin == target)
+           // break;
+           //cout<<' ' <<*begin;
+    };
+
+    // cout << "common " << common.c_str();
 };
