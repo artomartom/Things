78478d3b15d2ac71003f885375a4ec934306e29f
artom artom <1tematematema@gmail.com>
2022-07-15 00:07:52 +0300

    some more trees

--- tree/Tree.hpp
+++ tree/Tree.hpp
@@ -1,20 +1,24 @@
 #pragma once
 #include <memory>
 
-using Data_t = int;
-
 class Node final
 {
-    using NextNode = std::unique_ptr<Node>;
+    using Data_t = int;
+    using pNextNode_t = std::unique_ptr<Node>;
 
 public:
     Node() = default;
-    Node(Data_t &data)
+    Node(Data_t &&data)
+        : m_data{std::move(data)},
+          m_left{nullptr},
+          m_right{nullptr} {};
+
+    Node(const Data_t &data)
         : m_data{data},
           m_left{nullptr},
           m_right{nullptr} {};
 
-    static NextNode NewNode(Data_t &&data) { return std::make_unique<Node>(std::forward<Data_t>(data)); };
+    static pNextNode_t NewNode(Data_t &&data) { return std::make_unique<Node>(std::forward<Data_t>(data)); };
 
     // state: Get  funcs
     bool HasLeft() const noexcept { return m_left.get() != nullptr; };
@@ -29,11 +33,24 @@ public:
     // Set funcs
     void Left(Data_t &&data)
     {
-        m_left = NewNode(std::forward<Data_t>(data));
+        SetNodeData<&Node::HasLeft, &Node::m_left>(std::forward<Data_t>(data));
     };
     void Right(Data_t &&data)
     {
-        m_right = NewNode(std::forward<Data_t>(data));
+        SetNodeData<&Node::HasRight, &Node::m_right>(std::forward<Data_t>(data));
+    };
+
+    void Left(const Node &node){};
+    void Right(const Node &node){};
+    void Left(decltype(nullptr))
+    {
+        pNextNode_t tmp{nullptr};
+        std::swap(m_left, tmp);
+    };
+    void Right(decltype(nullptr))
+    {
+        pNextNode_t tmp{nullptr};
+        std::swap(m_right, tmp);
     };
 
     // Data: Set funcs
@@ -43,8 +60,19 @@ public:
     Data_t &Data() { return m_data; };
 
 private:
-    NextNode m_left{nullptr};
-    NextNode m_right{nullptr};
+    template <bool (Node::*Presence_Pred)() const noexcept, pNextNode_t(Node::*which_node)>
+    void SetNodeData(Data_t &&data)
+    {
+        if ((this->*Presence_Pred)())
+        {
+            (this->*which_node)->Data(std::forward<Data_t>(data));
+        }
+        else
+            (this->*which_node) = NewNode(std::forward<Data_t>(data));
+    };
+
+    pNextNode_t m_left{nullptr};
+    pNextNode_t m_right{nullptr};
     Data_t m_data{};
 };
 
